5584cd602407ff1780275deeda3d97f6587cd63c
LouwX <rulouw909@gmail.com>
2022-10-20 09:50:34 +0200

    Added a file for the main gui that needs to be built

---
+++ main.py
@@ -0,0 +1,7 @@
+# Ruan Louw
+# Scope:
+# 1. Create a system to store individual users and their data independently from each other
+# 2. Add a calendar planner
+# 3. Add notes/diet plans for each day
+# 4. Create a GUI to display all new information
+
